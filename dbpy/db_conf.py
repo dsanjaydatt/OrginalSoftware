cff539284a19ab5bb002a18e05d08ddabec39780
Sanjay Datt <dsanjaydatt@gmail.com>
2020-07-11 15:31:33 +0530

    changed user

--- dbpy/db_conf.py
+++ dbpy/db_conf.py
@@ -1,4 +1,4 @@
 import mysql.connector
 
 #open database connection
-db = mysql.connector.connect(host="localhost",user="dip",password="1234",database="shd_hospital")
+db = mysql.connector.connect(host="localhost",user="root",password="1234",database="shd_hospital")
